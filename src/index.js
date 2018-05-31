ff593cb538dc07180d81ed689401e824aafee848
kimdongwon <ehddnjs8989@gmail.com>
2018-05-31 15:11:36 +0900

    기봍세팅 끝

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-document.querySelector('h1').addEventListener('click', e => {
-  alert('Hello World!');
-});
+import axios from 'axios'
+
+const templates = {
+  menu: document.querySelector("#menu").content,
+  coupon: document.querySelector("#coupon"),content,
+  pay: document.querySelector("#pay").content,
+  menuLoading: document.querySelector("#menu_loading").content
+}
+
+async function menu(){
+  const fragment = document.importNode(templates.menu, true)
+  
+}
